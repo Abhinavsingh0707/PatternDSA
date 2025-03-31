40be8f621a3d3bb9bd61a61386f93e5e9f2608b8
Abhinavsingh0707 <abhinavsinghinright@gmail.com>
2025-03-31 12:10:10 +0530

    Add files via upload

---
+++ PatternDSACrack/TraversalInArray03.java
@@ -0,0 +1,12 @@
+// Do wHILE LOOP
+public class TraversalInArray03 {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5,6};
+        int i = 0;
+        do {
+            System.out.println(arr[i] + "");
+            i++;
+        } while (i < arr.length);
+    }
+    
+}
